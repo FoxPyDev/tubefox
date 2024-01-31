49b8382b6836e15d93aee3e73a00be33272fec9c
FoxPyDev <foxpythondev@gmail.com>
2024-02-01 01:13:14 +0200

    The Subtitles class works with a reference to the XML of video subtitles. Using the subtitles_to_text and subtitles_to_sub methods, subtitles are converted into convenient and familiar formats.

--- tubefox/subtitles.py
+++ tubefox/subtitles.py
@@ -1,4 +1,5 @@
 import html
+from helpers import TimeConvertor
 
 from bs4 import BeautifulSoup
 import requests
@@ -14,11 +15,19 @@ class Subtitles:
         subtitle = self.xml_subtitle.get_text(separator='\n')
         return html.unescape(subtitle)
 
-    def subtitles_to_stl(self):
-        pass
+    @property
+    def subtitles_to_sub(self):
+        text_blocks = self.xml_subtitle.findAll("text")
+        formatted_subtitles = ''
+        for block in text_blocks:
+            convertor = TimeConvertor(float(block['start']), float(block['dur']))
+            formatted_subtitles += (f'{text_blocks.index(block)+1}\n{convertor.convert_start_time} --> '
+                                    f'{convertor.convert_end_time}\n{html.unescape(block.text)}\n')
+        return formatted_subtitles
 
 
 if __name__ == "__main__":
     sbt = Subtitles("https://www.youtube.com/api/timedtext?v=HnjVSCnPYCU&ei=uK26ZaXtHZWB6dsPyf2bmA4&caps=asr&opi=112496729&xoaf=5&hl=uk&ip=0.0.0.0&ipbits=0&expire=1706758184&sparams=ip,ipbits,expire,v,ei,caps,opi,xoaf&signature=21E9A8A218B683A631516235BBAFFD1BA37254A5.79CBE56072BB4A9D1703A7EF113EE73CA60D03FA&key=yt8&kind=asr&lang=uk")
-    print(sbt.subtitles_to_text)
-    print(sbt.xml_subtitle)
+    # print(sbt.subtitles_to_text)
+    # print(sbt.xml_subtitle)
+    print(sbt.subtitles_to_sub)
