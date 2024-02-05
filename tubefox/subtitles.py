5973103d1238c365e5ee0285a472b5f6411056e0
FoxPyDev <foxpythondev@gmail.com>
2024-02-05 23:18:04 +0200

    Work in progress! lite fix

--- tubefox/subtitles.py
+++ tubefox/subtitles.py
@@ -27,7 +27,4 @@ class Subtitles:
 
 
 if __name__ == "__main__":
-    sbt = Subtitles("https://www.youtube.com/api/timedtext?v=HnjVSCnPYCU&ei=uK26ZaXtHZWB6dsPyf2bmA4&caps=asr&opi=112496729&xoaf=5&hl=uk&ip=0.0.0.0&ipbits=0&expire=1706758184&sparams=ip,ipbits,expire,v,ei,caps,opi,xoaf&signature=21E9A8A218B683A631516235BBAFFD1BA37254A5.79CBE56072BB4A9D1703A7EF113EE73CA60D03FA&key=yt8&kind=asr&lang=uk")
-    # print(sbt.subtitles_to_text)
-    # print(sbt.xml_subtitle)
-    print(sbt.subtitles_to_sub)
+    pass
