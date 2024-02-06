81f8f0ae20af106eb33ca02da9b38a0d4009d483
FoxPyDev <foxpythondev@gmail.com>
2024-02-06 15:21:18 +0200

    In the process of developing a base class

--- tubefox/__main__.py
+++ tubefox/__main__.py
@@ -1,12 +1,35 @@
 from web_scraper import WebScraper
 from app_scraper import AppScraper
-from metadata import Metadata
+from data_collector import DataCollector
 
 
 class TubeFox:
     def __init__(self, url):
         self.url = url
-        self.web_data_dict = WebScraper(self.url).get_data()
-        self.videoid = Metadata(self.web_data_dict).id
-        self.app_data_dict = AppScraper(self.videoid).get_data()
+        self.collected_data = DataCollector(WebScraper(self.url).data_dict)
+        self.app_data_dict = AppScraper(self.collected_data.collect_metadata()['id'])
 
+    @property
+    def id(self):
+        return self.collected_data.collect_metadata().get('id', '')
+
+    @property
+    def title(self):
+        return self.collected_data.collect_metadata().get('title', '')
+
+    @property
+    def description(self):
+        return self.collected_data.collect_metadata().get('description', '')
+
+    @property
+    def keywords(self):
+        keywords = self.collected_data.collect_metadata().get('keywords', [])
+        return ', '.join(keywords)
+
+
+if __name__ == "__main__":
+    yt = TubeFox('https://www.youtube.com/watch?v=CV7MuLeBKgE&ab_channel=AdrianZP')
+    print(yt.id)
+    print(yt.title)
+    print(yt.description)
+    print(yt.keywords)
