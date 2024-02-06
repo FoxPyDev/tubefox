6f5c44820540f15d722f15449278abf323b8606f
FoxPyDev <foxpythondev@gmail.com>
2024-02-06 15:20:45 +0200

    Code refactoring

--- tubefox/data_collector.py
+++ tubefox/data_collector.py
@@ -13,23 +13,26 @@ class DataCollector:
             'description': self.data_dict.get('videoDetails', {}).get('shortDescription', ''),
             'keywords': self.data_dict.get('videoDetails', {}).get('keywords', ''),
         }
-        print(video_info)
+        return video_info
 
-    def collect_subtitles(self):
+    def collect_subtitles_links(self):
         subtitles = self.data_dict.get('captions', {}).get('playerCaptionsTracklistRenderer', {}).get(
             'captionTracks', [])
         return {el.get('name', {}).get('simpleText', 'N/A'): el.get('baseUrl', '') for el in subtitles}
 
     def collect_video_links(self):
-        pass
+        videos = self.data_dict.get('streamingData', {}).get('formats', [])
+        return {link.get('height', 'N/A'): link.get('url', '') for link in videos}
 
     def collect_muted_video_links(self):
-        pass
+        muted_videos = self.data_dict.get('streamingData', {}).get('adaptiveFormats', [])
+        return {link.get('height', 'N/A'): link.get('url', '') for link in muted_videos if
+                "video/mp4" in link.get('mimeType', '')}
 
     def collect_audio_links(self):
         audios = self.data_dict.get('streamingData', {}).get('adaptiveFormats', [])
         return {link.get('bitrate', 'N/A'): link.get('url', '') for link in audios if
-                "audio/" in link.get('mimeType', '')}
+                "audio/mp4" in link.get('mimeType', '')}
 
     def collect_thumbnail_links(self):
         thumbnail_links = self.data_dict.get('videoDetails', {}).get('thumbnail', {}).get('thumbnails', [])
@@ -41,9 +44,11 @@ if __name__ == "__main__":
     app = AppScraper("VX-mp48z-Rg")
     data1 = DataCollector(web.data_dict)
     data2 = DataCollector(app.data_dict)
-    print(data1.collect_subtitles())
-    print(data2.collect_subtitles())
-    print(data1.collect_audio_links())
-    print(data2.collect_audio_links())
-    print(data1.collect_thumbnail_links())
-    print(data2.collect_thumbnail_links())
+    print(data1.collect_subtitles_links())
+    print(data2.collect_subtitles_links())
+    # print(data1.collect_audio_links())
+    # print(data2.collect_audio_links())
+    # print(data1.collect_thumbnail_links())
+    # print(data2.collect_thumbnail_links())
+    # print(data1.collect_muted_video_links())
+    # print(data2.collect_muted_video_links())
