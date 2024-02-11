dff65f99acd8c6ed0457568183d9315e3b3d9238
FoxPyDev <foxpythondev@gmail.com>
2024-02-11 19:14:01 +0200

    Add documentation and typing

--- tubefox/downloader.py
+++ tubefox/downloader.py
@@ -3,15 +3,48 @@ from tqdm import tqdm
 
 
 class Downloader:
-    def __init__(self, path, filename, file_type, download_link, extension, chunk_size=1024):
-        self.path = path
-        self.filename = filename
-        self.file_type = file_type
-        self.download_link = download_link
-        self.extension = extension
-        self.chunk_size = chunk_size
+    """
+    Downloader class for downloading files from URLs.
 
-    def download_file(self):
+    Args:
+        path (str): The path where the downloaded file will be saved.
+        filename (str): The name of the downloaded file.
+        file_type (str): The type of the downloaded file (e.g., 'video', 'audio', 'thumbnail').
+        download_link (str): The URL from which the file will be downloaded.
+        extension (str): The file extension of the downloaded file.
+        chunk_size (int, optional): The size of each chunk in bytes for streaming. Defaults to 1024.
+
+    Attributes:
+        path (str): The path where the downloaded file will be saved.
+        filename (str): The name of the downloaded file.
+        file_type (str): The type of the downloaded file.
+        download_link (str): The URL from which the file will be downloaded.
+        extension (str): The file extension of the downloaded file.
+        chunk_size (int): The size of each chunk in bytes for streaming.
+
+    Methods:
+        download_file: Download the file from the provided URL and save it to the specified path.
+
+    Note:
+        This class utilizes the requests library for making HTTP requests and tqdm for displaying progress bars.
+    """
+
+    def __init__(self, path: str, filename: str, file_type: str, download_link: str, extension: str,
+                 chunk_size: int = 1024) -> None:
+        self.path: str = path
+        self.filename: str = filename
+        self.file_type: str = file_type
+        self.download_link: str = download_link
+        self.extension: str = extension
+        self.chunk_size: int = chunk_size
+
+    def download_file(self) -> None:
+        """
+        Download the file from the provided URL and save it to the specified path.
+
+        Returns:
+            None
+        """
         # Check if the link is empty or None
         if not self.download_link:
             print(f"No {self.file_type} download link available.")
