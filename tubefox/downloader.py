891c340829478f48799f4c8aed7b865d4c35931b
FoxPyDev <foxpythondev@gmail.com>
2024-02-11 18:36:34 +0200

    Lite refactoring

--- tubefox/downloader.py
+++ tubefox/downloader.py
@@ -3,21 +3,21 @@ from tqdm import tqdm
 
 
 class Downloader:
-    def __init__(self, path, filename, file_type, best_quality_link, extension, chunk_size=1024):
+    def __init__(self, path, filename, file_type, download_link, extension, chunk_size=1024):
         self.path = path
         self.filename = filename
         self.file_type = file_type
-        self.best_quality_link = best_quality_link
+        self.download_link = download_link
         self.extension = extension
         self.chunk_size = chunk_size
 
-    def _download_file(self):
+    def download_file(self):
         # Check if the link is empty or None
-        if not self.best_quality_link:
+        if not self.download_link:
             print(f"No {self.file_type} download link available.")
             return
 
-        response = requests.get(self.best_quality_link, stream=True)
+        response = requests.get(self.download_link, stream=True)
         total_size = int(response.headers.get('content-length', 0))
         print(f"Start download {self.file_type}")
         with open(f'{self.path}{self.filename}.{self.extension}', 'wb') as file, tqdm(
@@ -28,7 +28,3 @@ class Downloader:
                     file.write(chunk)
                     progress_bar.update(len(chunk))
         print(f"{self.file_type} downloaded")
-
-
-if __name__ == "__main__":
-    pass
