2093b071660457f3e7da1ffdcc02dc02d7bc09e5
FoxPyDev <foxpythondev@gmail.com>
2024-02-11 18:44:30 +0200

    Add documentation and typing

--- tubefox/tubefox.py
+++ tubefox/tubefox.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from web_scraper import WebScraper
 from app_scraper import AppScraper
 from data_collector import DataCollector
@@ -8,29 +10,53 @@ from helpers import clean_filename
 
 
 class TubeFox:
-    def __init__(self, url):
-        self.url = url
-        self.web_collected_data = DataCollector(WebScraper(self.url).data_dict)
-        self.app_collected_data = DataCollector(AppScraper(self.web_collected_data.collect_metadata()['id']).data_dict)
+    """
+    TubeFox is a wrapper class that provides convenient methods for scraping data, downloading media,
+    and managing subtitles from YouTube videos.
+
+    Args:
+        url (str): The URL of the YouTube video.
+    """
+
+    def __init__(self, url: str) -> None:
+        self.url: str = url
+        self.web_collected_data: DataCollector = DataCollector(WebScraper(self.url).data_dict)
+        self.app_collected_data: DataCollector = DataCollector(
+            AppScraper(self.web_collected_data.collect_metadata()['id']).data_dict
+        )
 
     @property
-    def id(self):
+    def id(self) -> str:
+        """Return the video ID."""
         return self.web_collected_data.collect_metadata().get('id', '')
 
     @property
-    def title(self):
+    def title(self) -> str:
+        """Return the video title."""
         return self.web_collected_data.collect_metadata().get('title', '')
 
     @property
-    def description(self):
+    def description(self) -> str:
+        """Return the video description."""
         return self.web_collected_data.collect_metadata().get('description', '')
 
     @property
-    def keywords(self):
+    def keywords(self) -> str:
+        """Return the video keywords as a comma-separated string."""
         keywords = self.web_collected_data.collect_metadata().get('keywords', [])
         return ', '.join(keywords)
 
-    def download_video(self, filename=None, path=None, quality=None):
+    def download_video(self, filename: Optional[str] = None, path: Optional[str] = None,
+                       quality: Optional[int] = None) -> None:
+        """
+        Download the video.
+
+        Args:
+            filename (str, optional): The name of the downloaded video file. If not provided,
+            the video title will be used.
+            path (str, optional): The path where the downloaded file will be saved. Defaults to current directory.
+            quality (int, optional): The desired quality of the video. Defaults to the highest available quality.
+        """
         best_quality_link = yt.app_collected_data.collect_video_links()[max(
             yt.app_collected_data.collect_video_links().keys())]
         if quality is None:
@@ -48,9 +74,19 @@ class TubeFox:
                    file_type='video',
                    extension='mp4').download_file()
 
-    def download_muted_video(self, filename=None, path=None, quality=None):
+    def download_muted_video(self, filename: Optional[str] = None, path: Optional[str] = None,
+                             quality: Optional[int] = None) -> None:
+        """
+        Download the video without audio.
+
+        Args:
+            filename (str, optional): The name of the downloaded video file. If not provided,
+            the video title will be used.
+            path (str, optional): The path where the downloaded file will be saved. Defaults to current directory.
+            quality (int, optional): The desired quality of the video. Defaults to the highest available quality.
+        """
         best_quality_link = yt.app_collected_data.collect_muted_video_links()[max(
-                yt.app_collected_data.collect_muted_video_links().keys())]
+            yt.app_collected_data.collect_muted_video_links().keys())]
         if quality is None:
             download_link = best_quality_link
         else:
@@ -66,9 +102,19 @@ class TubeFox:
                    file_type='video',
                    extension='mp4').download_file()
 
-    def download_audio(self, filename=None, path=None, quality=None):
+    def download_audio(self, filename: Optional[str] = None, path: Optional[str] = None,
+                       quality: Optional[int] = None) -> None:
+        """
+        Download the audio track of the video.
+
+        Args:
+            filename (str, optional): The name of the downloaded audio file. If not provided,
+            the video title will be used.
+            path (str, optional): The path where the downloaded file will be saved. Defaults to current directory.
+            quality (int, optional): The desired quality of the audio. Defaults to the highest available quality.
+        """
         best_quality_link = yt.app_collected_data.collect_audio_links()[max(
-                yt.app_collected_data.collect_audio_links().keys())]
+            yt.app_collected_data.collect_audio_links().keys())]
         if quality is None:
             download_link = best_quality_link
         else:
@@ -84,9 +130,19 @@ class TubeFox:
                    file_type='audio',
                    extension='mp3').download_file()
 
-    def download_thumbnail(self, filename=None, path=None, quality=None):
+    def download_thumbnail(self, filename: Optional[str] = None, path: Optional[str] = None,
+                           quality: Optional[int] = None) -> None:
+        """
+        Download the video thumbnail.
+
+        Args:
+            filename (str, optional): The name of the downloaded thumbnail file. If not provided,
+            the video title will be used.
+            path (str, optional): The path where the downloaded file will be saved. Defaults to current directory.
+            quality (int, optional): The desired quality of the thumbnail. Defaults to the highest available quality.
+        """
         best_quality_link = yt.web_collected_data.collect_thumbnail_links()[max(
-                yt.web_collected_data.collect_thumbnail_links().keys())]
+            yt.web_collected_data.collect_thumbnail_links().keys())]
         if quality is None:
             download_link = best_quality_link
         else:
@@ -104,7 +160,17 @@ class TubeFox:
             file_type='thumbnail',
             extension='jpg').download_file()
 
-    def download_subtitles(self, mode=None, filename=None, path=None):
+    def download_subtitles(self, mode: Optional[str] = None, filename: Optional[str] = None,
+                           path: Optional[str] = None) -> None:
+        """
+        Download subtitles of the video.
+
+        Args:
+            mode (str, optional): The format of subtitles to download ('srt' or 'txt'). Defaults to 'txt'.
+            filename (str, optional): The name of the downloaded subtitles file. If not provided,
+            the video title will be used.
+            path (str, optional): The path where the downloaded file will be saved. Defaults to current directory.
+        """
         if path is None:
             path = "./"
         if filename is None:
