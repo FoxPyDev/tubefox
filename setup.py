20cbd12e65bef4bb71962c012e2796bb123fa86a
FoxPyDev <foxpythondev@gmail.com>
2024-02-12 11:24:01 +0200

    Bug fix

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/FoxPyDev/tubefox",
     packages=find_packages(),
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
