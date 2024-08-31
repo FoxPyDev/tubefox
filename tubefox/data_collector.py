0361d84a17292667e4698e1e4d50f67c7d4dde00
FoxPyDev <foxpythondev@gmail.com>
2024-08-31 15:18:23 +0300

    Updated Data Collector.

--- tubefox/data_collector.py
+++ tubefox/data_collector.py
@@ -21,6 +21,12 @@ class DataCollector:
     """
 
     def __init__(self, data_dict: dict) -> None:
+        """
+        Initializes the DataCollector with a data dictionary.
+
+        Args:
+            data_dict (dict): A dictionary containing data related to a YouTube video.
+        """
         self.data_dict: dict = data_dict
 
     def collect_metadata(self) -> dict:
@@ -49,44 +55,96 @@ class DataCollector:
             'captionTracks', [])
         return {el.get('name', {}).get('simpleText', 'N/A'): el.get('baseUrl', '') for el in subtitles}
 
+    def collect_muted_video_links(self) -> dict:
+        """
+        Collect muted video links from the streaming data.
+
+        Returns:
+            dict: A dictionary mapping video heights to dictionaries with video information.
+                  Example:
+                  {
+                      720: {
+                          'url': 'http://example.com/video1',
+                          'mimeType': 'video/mp4; codecs="av01.0"',
+                          'format': 'mp4'
+                      },
+                      1080: {
+                          'url': 'http://example.com/video2',
+                          'mimeType': 'video/webm; codecs="vp9"',
+                          'format': 'webm'
+                      }
+                  }
+        """
+        videos = self.data_dict.get('streamingData', {}).get('adaptiveFormats', [])
+        result = {}
+        for video in videos:
+            if 'video/mp4; codecs="av01.0' in video['mimeType']:
+                format_type = "mp4"
+            elif 'video/webm; codecs="vp9' in video['mimeType']:
+                format_type = "webm"
+            else:
+                continue
+
+            result[video['height']] = {
+                'url': video['url'],
+                'mimeType': video['mimeType'],
+                'format': format_type
+            }
+        return result
+
     def collect_video_links(self) -> dict:
         """
         Extract links to video streams of different qualities.
 
         Returns:
-            dict: A dictionary mapping video qualities to their corresponding URLs.
+            dict: A dictionary mapping video heights to dictionaries with video information.
         """
         videos = self.data_dict.get('streamingData', {}).get('formats', [])
-        return {link.get('height', 'N/A'): link.get('url', '') for link in videos}
-
-    def collect_muted_video_links(self) -> dict:
-        """
-        Extract links to muted video streams of different qualities.
-
-        Returns:
-            dict: A dictionary mapping video qualities to their corresponding URLs.
-        """
-        muted_videos = self.data_dict.get('streamingData', {}).get('adaptiveFormats', [])
-        return {link.get('height', 'N/A'): link.get('url', '') for link in muted_videos if
-                "video/mp4" in link.get('mimeType', '')}
+        result = {}
+        for video in videos:
+            result[video['height']] = {
+                'url': video['url'],
+                'mimeType': video['mimeType'],
+                'format': 'mp4',
+                'full_video': 'ok',
+            }
+        return result
 
     def collect_audio_links(self) -> dict:
         """
         Extract links to audio streams of different qualities.
 
         Returns:
-            dict: A dictionary mapping audio qualities to their corresponding URLs.
+            dict: A dictionary mapping bitrates to dictionaries with audio information.
         """
         audios = self.data_dict.get('streamingData', {}).get('adaptiveFormats', [])
-        return {link.get('bitrate', 'N/A'): link.get('url', '') for link in audios if
-                "audio/mp4" in link.get('mimeType', '')}
+        result = {}
+        for audio in audios:
+            if 'mp4a' in audio['mimeType']:
+                format_type = "mp4a"
+            elif 'opus' in audio['mimeType']:
+                format_type = "opus"
+            else:
+                continue
+
+            result[audio['bitrate']] = {
+                'url': audio['url'],
+                'mimeType': audio['mimeType'],
+                'format': format_type
+            }
+        return result
 
     def collect_thumbnail_links(self) -> dict:
         """
         Extract links to thumbnails of different sizes.
 
         Returns:
-            dict: A dictionary mapping thumbnail sizes to their corresponding URLs.
+            dict: A dictionary mapping thumbnail sizes to their corresponding URLs with format 'jpg'.
         """
         thumbnail_links = self.data_dict.get('videoDetails', {}).get('thumbnail', {}).get('thumbnails', [])
-        return {thumbnail.get('height', 'N/A'): thumbnail.get('url', '') for thumbnail in thumbnail_links}
+        return {
+            thumbnail.get('height', 'N/A'): {
+                'url': thumbnail.get('url', ''),
+                'format': 'jpg'
+            } for thumbnail in thumbnail_links
+        }
